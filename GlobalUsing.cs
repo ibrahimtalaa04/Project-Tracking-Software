1983bc6551226f599d8c1a061f9e6adb29d0f5cb
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-21 11:30:39 +0300

    update user model

---
+++ GlobalUsing.cs
@@ -0,0 +1,10 @@
+﻿global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.Xml.Linq;
+
+namespace ProjectTrackingSoftware
+{
+    public class GlobalUsing
+    {
+    }
+}
