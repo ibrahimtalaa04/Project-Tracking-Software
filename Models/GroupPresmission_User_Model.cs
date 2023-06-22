c86042140a5fc56975f6333499397214ce7f8395
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-22 11:09:23 +0300

    update group premission user model

---
+++ Models/GroupPresmission_User_Model.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class GroupPresmission_User_Model
+    {
+        public int GroupID { get; set; }
+        public int UserId { get; set; }
+    }
+}
