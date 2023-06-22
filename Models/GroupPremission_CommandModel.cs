2def83025b9a00691a0d394a677bd123677087ab
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-22 10:40:38 +0300

    update group premission command model

---
+++ Models/GroupPremission_CommandModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class GroupPremission_CommandModel
+    {
+        public int CommandId { get; set; }
+        public int GroupPremissionId { get; set; }
+    }
+}
