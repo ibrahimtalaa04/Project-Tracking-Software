6df44b81b60581313e0da7900668c05877d2a828
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-22 10:37:52 +0300

    update screen model

---
+++ Models/ScreenModel.cs
@@ -0,0 +1,25 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class ScreenModel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Display(Name = "Title")]
+        [Required]
+        public string? Title { get; set; }
+
+
+        [MaxLength(50)]
+        [Display(Name = "Path")]
+        [Required]
+        public string? Path { get; set; }
+
+        [MaxLength(100, ErrorMessage = "Description must be less than {0}")]
+        [Display(Name = "Description")]
+        [Required(ErrorMessage = "Description is required")]
+        public string? Description { get; set; }
+    }
+}
