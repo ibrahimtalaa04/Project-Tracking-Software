ad8e949773c281ef982a37e6120b6d4ed36a640f
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-22 09:21:45 +0300

    update group premission model

---
+++ Models/GroupPremissionModel.cs
@@ -0,0 +1,20 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class GroupPremissionModel
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+
+        [MaxLength(50,ErrorMessage ="Group title must be less than {0}")]
+        [Display(Name = "Group Title")]
+        [Required(ErrorMessage ="Group Title is required")]
+        public string? Title { get; set; }
+
+        [Display(Name = "Description")]
+        [Required(ErrorMessage ="Description is required")]
+        public string? Description { get; set; }
+    }
+}
