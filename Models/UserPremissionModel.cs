95a2ee21cbd38854d53bb32095e863fe5c3c54be
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-22 10:38:00 +0300

    update user premission model

---
+++ Models/UserPremissionModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class UserPremissionModel
+    {
+        public int UserId { get; set; }
+
+        public int ScreenId { get; set; }
+
+        public int CommandId { get; set; }
+
+    }
+
+}
