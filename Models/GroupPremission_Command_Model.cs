449845ad11afdfdd6d894a6c32a827a6320fd1b6
ibrahimtalaa04 <ibrahim6tal@gmail.com>
2023-06-25 11:38:47 +0300

    hands on models

---
+++ Models/GroupPremission_Command_Model.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class GroupPremission_Command_Model
+    {
+        public int CommandId { get; set; }
+        public int GroupPremissionId { get; set; }
+    }
+}
---
+++ Models/User_Project_Task_Model.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectTrackingSoftware.Models
+{
+    public class User_Project_Task_Model
+    {
+        public int UserId { get; set; }
+        public int ProjectId { get; set; }
+        public int TaskId { get; set; }
+    }
+}
